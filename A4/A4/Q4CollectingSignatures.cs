4121809cadf02c2eb3a3a46b0f9a3659721f6b03
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-19 10:09:36 +0330

    q4

--- A4/A4/Q4CollectingSignatures.cs
+++ A4/A4/Q4CollectingSignatures.cs
@@ -17,7 +17,38 @@ namespace A4
 
         public virtual long Solve(long tenantCount, long[] startTimes, long[] endTimes)
         {
-            throw new NotImplementedException();
+            List<Segment> segments = GetSortedSegments(tenantCount, startTimes, endTimes);
+            long count = 0;
+            while (segments.Count > 0)
+            {
+                Segment firstSegment = new Segment(segments[0].StartPoint,
+                                                   segments[0].EndPoint);
+                bool hasIntersection = false;
+                foreach (Segment segment in segments)
+                {
+                    if (segment.StartPoint <= firstSegment.EndPoint &&
+                        segment.EndPoint >= firstSegment.EndPoint)
+                    {
+                        hasIntersection = true;
+                        segments[segments.IndexOf(segment)].Checked = true;
+                    }
+                }
+                count += (hasIntersection) ? 1 : 0;
+                segments = segments.Where(s => !(s.Checked)).ToList();
+            }
+            return count;
+        }
+
+        //GetSortedSegments method returning the sorted segments(sorted by endPoints)
+        private List<Segment> GetSortedSegments(long tenantCount, long[] startTimes, long[] endTimes)
+        {
+            List<Segment> segments = new List<Segment>();
+            for (int i = 0; i < tenantCount; i++)
+            {
+                segments.Add(new Segment(startTimes[i], endTimes[i]));
+            }
+            segments = segments.OrderByDescending(s => s.EndPoint).Reverse().ToList();
+            return segments;
         }
     }
 }
---
+++ A4/A4/Segment.cs
@@ -0,0 +1,14 @@
+﻿namespace A4
+{
+    public class Segment
+    {
+        public long StartPoint { get; set; }
+        public long EndPoint { get; set; }
+        public bool Checked { get; set; }
+        public Segment(long start, long end)
+        {
+            StartPoint = start;
+            EndPoint = end;
+        }
+    }
+}
