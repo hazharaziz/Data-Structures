df07ca7f4d3791d35e87f8d17caadf0c7affb58e
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-19 10:04:47 +0330

    q1

--- A4/A4/Q1ChangingMoney.cs
+++ A4/A4/Q1ChangingMoney.cs
@@ -16,7 +16,18 @@ namespace A4
 
         public virtual long Solve(long money)
         {
-            throw new NotImplementedException();
+            int[] coins = { 10, 5, 1 };
+            long count = 0;
+            int index = 0;
+            long subtractValue = 0;
+            while (money > 0)
+            {
+                subtractValue = money - (money % coins[index]);
+                count += (coins[index] != 1) ? subtractValue / coins[index] : 1;
+                money -= (coins[index] != 1) ? subtractValue : 1;
+                index += (coins[index] != 1) ? 1 : 0;
+            }
+            return count;
         }
     }
 }
