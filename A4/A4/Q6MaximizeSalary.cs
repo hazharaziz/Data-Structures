0eb9d195a5e74cc722c55f31ce2f576ac2aa8394
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-19 10:11:38 +0330

    q6

--- A4/A4/Q6MaximizeSalary.cs
+++ A4/A4/Q6MaximizeSalary.cs
@@ -18,7 +18,40 @@ namespace A4
 
         public virtual string Solve(long n, long[] numbers)
         {
-            throw new NotImplementedException();
+            List<long> nums = numbers.ToList();
+            string result = "";
+            long count = 0;
+            while (count < n)
+            {
+                long maxDigit = GetMaxNumber(nums);
+                nums.RemoveAt(nums.IndexOf(maxDigit));
+                result += maxDigit;
+                count++;
+            }
+            return result;
+        }
+
+        //GetMaxNumber method returning the maximum number of the list by specific comparison
+        private long GetMaxNumber(List<long> numbers)
+        {
+            long maxDigit = 0;
+            foreach (long number in numbers)
+                if (IsGreaterOrEqual(number, maxDigit))
+                {
+                    maxDigit = number;
+                }
+
+            return maxDigit;
+        }
+
+        //IsGreaterOrEqual method checking the greatness of two numbers by checking their permutations
+        private bool IsGreaterOrEqual(long number, long maxDigit)
+        {
+            bool result = false;
+            string n1 = $"{number}{maxDigit}";
+            string n2 = $"{maxDigit}{number}";
+            result = (long.Parse(n1) - long.Parse(n2) >= 0) ? true : false;
+            return result;
         }
     }
 }
