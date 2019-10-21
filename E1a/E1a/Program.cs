1172d53cd19dc12216f37cd7df2513f0f935a381
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 10:26:12 +0330

    project initialized

---
+++ E1a/E1a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
