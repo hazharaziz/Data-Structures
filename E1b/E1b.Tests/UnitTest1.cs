b897ec2467191c84d171c2811f029ea4bae43c53
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 13:16:06 +0330

    project initialized

---
+++ E1b/E1b.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace E1b.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ E1b/E1b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E1b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
