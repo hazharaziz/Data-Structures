0ff8a6827c7340db78271424b1cef1ce7234637f
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 10:57:29 +0330

    q1

--- E1a/E1a/Q1Stones.cs
+++ E1a/E1a/Q1Stones.cs
@@ -11,10 +11,27 @@ namespace E1a
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<long, long[], long>)Solve);
 
-
+        
         public virtual long Solve(long n, long[] stones)
         {
-            throw new NotImplementedException();
+            long resultDay = 0;
+            long stoneCount = 0;
+
+            while (stoneCount <= n)
+            {
+                if (resultDay >= stones.Length)
+                {
+                    resultDay = 0;
+                    break;
+                }
+
+                stoneCount += stones[resultDay];
+                resultDay++;
+
+                if (stoneCount >= n)
+                    break;
+            }
+            return resultDay;
         }
     }
 }
