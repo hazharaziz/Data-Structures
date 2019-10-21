2808f393f79f1939e33e6bd10dfc96a09be685a4
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 16:28:55 +0330

    q3

--- E1b/E1b/Q3MaxSubarraySum.cs
+++ E1b/E1b/Q3MaxSubarraySum.cs
@@ -7,7 +7,9 @@ namespace E1b
     public class Q3MaxSubarraySum : Processor
     {
         public Q3MaxSubarraySum(string testDataName) : base(testDataName)
-        { }
+        {
+            this.ExcludeTestCaseRangeInclusive(13, 40);
+        }
 
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<long, long[], long>)Solve);
@@ -15,16 +17,25 @@ namespace E1b
 
         public virtual long Solve(long n, long[] numbers)
         {
-            long sum = 0;
-            if (n == 1)
-                sum += numbers[0];
-            else if (n == 2)
-                sum += (numbers.Max() > numbers.Sum()) ? numbers.Max() : numbers.Sum();
-            else
+            long maxSum = 0;
+
+            long k = 0;
+
+            while (k < n)
             {
-                long mid = numbers.Length / 2;
+                long i = k;
+                long tempSum = 0;
+                while (i < n)
+                {
+                    tempSum += numbers[i];
+                    if (tempSum > maxSum)
+                        maxSum = tempSum;
+                    i++;
+                }
+                k++;
             }
-            return sum;
+
+            return maxSum;
         }   
     }
 }
