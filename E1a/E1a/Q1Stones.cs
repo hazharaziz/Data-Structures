1d4979742ff8e6803c35090e9d9cf97869d91c69
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 10:28:29 +0330

    project files added

---
+++ E1a/E1a/Q1Stones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestCommon;
+
+namespace E1a
+{
+    public class Q1Stones : Processor
+    {
+        public Q1Stones(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+
+        public virtual long Solve(long n, long[] stones)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
