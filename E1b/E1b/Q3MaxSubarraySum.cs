72e27ad16e153bca7854c4a372295aae04f0c9a0
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-21 13:18:06 +0330

    project files added

---
+++ E1b/E1b/Q3MaxSubarraySum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestCommon;
+
+namespace E1b
+{
+    public class Q3MaxSubarraySum : Processor
+    {
+        public Q3MaxSubarraySum(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], long>)Solve);
+
+
+        public virtual long Solve(long n, long[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ E1b/E1b/Q4HungryFrog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestCommon;
+
+namespace E1b
+{
+    public class Q4HungryFrog : Processor
+    {
+        public Q4HungryFrog(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long[][], long>)Solve);
+
+
+        public virtual long Solve(long n, long p, long[][] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
