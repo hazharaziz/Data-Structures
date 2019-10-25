bbad886730ea07d42b0827838f2ff506a2431576
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-25 11:20:38 +0330

    project files added

---
+++ E1c/E1c/Q2UnitFractions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace E1c
+{
+    public class Q2UnitFractions : Processor
+    {
+        public Q2UnitFractions(string testDataName) : base(testDataName)
+        { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long, long>)Solve);
+
+
+        public virtual long Solve(long nr, long dr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
