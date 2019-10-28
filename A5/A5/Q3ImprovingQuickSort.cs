07bfe2d9a4673e0ba4088580bcfab9c292c7d7d4
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-10-28 11:57:25 +0330

    q3

--- A5/A5/Q3ImprovingQuickSort.cs
+++ A5/A5/Q3ImprovingQuickSort.cs
@@ -15,8 +15,44 @@ namespace A5
 
         public virtual long[] Solve(long n, long[] a)
         {
-            //write your code here
-            throw new NotImplementedException();
+            QuickSort(a, 0, n - 1);
+            return a;
+        }
+
+        private void QuickSort(long[] arr, long low, long high)
+        {
+            if (low < high)
+            {
+                long[] partitions = Partition(arr, low, high);
+                long leftPartition = partitions[0];
+                long rightPartition = partitions[1];
+                QuickSort(arr, low, leftPartition - 1);
+                QuickSort(arr, rightPartition + 1, high);
+            }
+        }
+
+        private long[] Partition(long[] arr, long low, long high)
+        {
+            long leftPartition = low;
+            long rightPartition = high;
+            long pivot = arr[low];
+            for (long i = low; i <= high; i++)
+            {
+                if (arr[i] < pivot)
+                {
+                    (arr[i], arr[leftPartition]) = (arr[leftPartition], arr[i]);
+                    leftPartition++;
+                }
+            }
+            for (long i = high; i >= low; i--)
+            {
+                if (arr[i] > pivot)
+                {
+                    (arr[i], arr[rightPartition]) = (arr[rightPartition], arr[i]);
+                    rightPartition--;
+                }
+            }
+            return new long[] { leftPartition, rightPartition };
         }
     }
 }
