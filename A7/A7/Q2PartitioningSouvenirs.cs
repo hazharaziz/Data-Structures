d874a0f7cc86cfe8c54de9bc1d33ba41981d6fe4
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-11-14 22:46:50 +0330

    q2

--- A7/A7/Q2PartitioningSouvenirs.cs
+++ A7/A7/Q2PartitioningSouvenirs.cs
@@ -8,7 +8,8 @@ namespace A7
 {
     public class Q2PartitioningSouvenirs : Processor
     {
-        public Q2PartitioningSouvenirs(string testDataName) : base(testDataName) { }
+        public Q2PartitioningSouvenirs(string testDataName) : base(testDataName)
+        { }
 
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<long, long[], long>)Solve);
@@ -17,43 +18,45 @@ namespace A7
         {
             bool result = false;
             long sum = souvenirs.Sum();
-            if (sum % 3 != 0)
+            if (sum % 3 != 0 || sum == 0 || souvenirsCount < 3)
                 result = false;
             else
             {
-                long[,,] matrix = MatrixInit(souvenirsCount, sum);
+                bool[,,] matrix = MatrixInit(souvenirsCount, sum);
                 for (long i = 1; i <= souvenirsCount; i++)
                 {
-                    for (long j = 1; j <= sum / 3; j++)
+                    for (long j = 0; j <= sum / 3; j++)
                     {
-                        for (long k = 1; k <= sum / 3; k++)
+                        for (long k = 0; k <= sum / 3; k++)
                         {
-                            if (matrix[i,] ) 
+                            bool[] booleans = new bool[4];
+                            if (souvenirs[i - 1] <= j && souvenirs[i - 1] <= k)
+                                booleans[0] = matrix[i - 1, j - souvenirs[i - 1], k] ||
+                                              matrix[i - 1, j, k - souvenirs[i - 1]];
+                            if (souvenirs[i - 1] <= j && souvenirs[i - 1] > k)
+                                booleans[1] = matrix[i - 1, j - souvenirs[i - 1], k];
+                            if (souvenirs[i - 1] > j && souvenirs[i - 1] <= k)
+                                booleans[2] = matrix[i - 1, j, k - souvenirs[i - 1]];
+                            if (souvenirs[i - 1] > j && souvenirs[i - 1] > k)
+                                booleans[3] = matrix[i - 1, j, k];
+                            matrix[i, j, k] = (booleans.Any(b => b == true)) ? true : false;
                         }
                     }
                 }
+                result = matrix[souvenirsCount, sum / 3, sum / 3];
             }
             return (result) ? 1 : 0;
         }
 
-        private long[,,] MatrixInit(long souvenirsCount, long sum)
+        private bool[,,] MatrixInit(long souvenirsCount, long sum)
         {
-            long[,,] result = new long[souvenirsCount + 1, sum / 3 + 1, sum / 3 + 1];
+            bool[,,] result = new bool[souvenirsCount + 1, sum / 3 + 1, sum / 3 + 1];
             for (long i = 0; i <= souvenirsCount; i++)
-            {
-                result[i, 0, 0] = 1;
-            }
-            return result;
-        }
-
-        private long[][] GetSubsets(long[] souvenirs)
-        {
-            long[][] result = new long[souvenirs.Length + 1][];
-            for (long i = 0; i < result.Length; i++)
-            {
-                result[i] = new long[i];
-
-            }
+                result[i, 0, 0] = true;
+            for (long i = 1; i <= sum / 3; i++)
+                result[0, i, 0] = true;
+            for (long i = 1; i <= sum / 3; i++)
+                result[0, 0, i] = true;
             return result;
         }
     }
