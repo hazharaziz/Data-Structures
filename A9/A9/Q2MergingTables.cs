29ff754606ab1e19d39ce537ba57456269337628
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-12-04 13:09:51 +0330

    q2

--- A9/A9/Q2MergingTables.cs
+++ A9/A9/Q2MergingTables.cs
@@ -16,10 +16,44 @@ namespace A9
             TestTools.Process(inStr, (Func<long[], long[], long[], long[]>)Solve);
 
 
-        public long[] Solve(long[] tableSizes, long[] targetTables, long[] sourceTables)
+        public long[] Solve(long[] tableSizes, long[] targetTables, 
+                                                long[] sourceTables)
         {
-            throw new NotImplementedException();
+            long max = tableSizes.Max();
+            long length = tableSizes.Length;
+            long[] maxSizes = new long[targetTables.Length];
+            long tempSize;
+            long source;
+            long dest;
+            parent = new long[length];
+            for (long i = 0; i < length; i++)
+                parent[i] = i + 1;
+
+            rank = tableSizes;
+
+            for (long i = 0; i < targetTables.Length; i++)
+            {
+                source = Find(sourceTables[i]);
+                dest = Find(targetTables[i]);
+                if (source != dest)
+                {
+                    rank[dest - 1] += rank[source - 1];
+                    rank[source - 1] = 0;
+                    parent[source - 1] = dest;
+                }
+                tempSize = rank[dest - 1];
+
+                max = (tempSize > max) ? tempSize : max;
+                maxSizes[i] = max;
+            }
+            return maxSizes;
         }
 
+        public long Find(long i)
+        {
+            while (i != parent[i - 1])
+                i = parent[i - 1];
+            return i;
+        }
     }
 }
