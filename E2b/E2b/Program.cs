01f829ab6cd9673bf8c6f13445bfb1754ea1dd36
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-12-23 12:56:09 +0330

    project init

---
+++ E2b/E2b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
