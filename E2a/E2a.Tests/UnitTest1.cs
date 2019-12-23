f1c9db4cad825a461956637f2e1f9e51851da4b6
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-12-23 10:11:14 +0330

    project init

---
+++ E2a/E2a.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace E2a.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
