7e25306d50f1032c74bb0cca6996ccc94bfc1e3d
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-12-23 12:58:18 +0330

    project files added

---
+++ E2b/E2b/Q1ImplementNextForBST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestCommon;
+
+namespace E2b
+{
+    public class Q1ImplementNextForBST : Processor
+    {
+        public Q1ImplementNextForBST(string testDataName) : base(testDataName) 
+        {
+            //this.ExcludeTestCaseRangeInclusive(1, 10);
+        }
+        public override string Process(string inStr)
+        {
+            long n, node;
+            var lines = inStr.Split(TestTools.NewLineChars, StringSplitOptions.RemoveEmptyEntries);
+            TestTools.ParseTwoNumbers(lines[0], out n, out node);
+            var bst = lines[1].Split(TestTools.IgnoreChars, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => long.Parse(x))
+                .ToArray();
+
+            return Solve(n, node, bst).ToString();
+        }
+
+        public long Solve(long n, long node, long[] BST)
+        {
+            throw new NotImplementedException();            
+        }
+    }
+}
---
+++ E2b/E2b/Q2HashTableAttack.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TestCommon;
+
+namespace E2b
+{
+    public class Q2HashTableAttack : Processor
+    {
+        public Q2HashTableAttack (string testDataName) : base(testDataName) 
+        {
+        }
+
+        public override string Process(string inStr)
+        {
+            long bucketCount = long.Parse(inStr);
+            return string.Join("\n", Solve(bucketCount));
+        }
+
+        public string[] Solve(long bucketCount)
+        {
+            throw new NotImplementedException();            
+        }
+
+        #region Chars
+        static char[] LowChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('a' + n))
+            .ToArray();
+
+        static char[] CapChars = Enumerable
+            .Range(0, 26)
+            .Select(n => (char)('A' + n))
+            .ToArray();
+
+        static char[] Numbers = Enumerable
+            .Range(0, 10)
+            .Select(n => (char)('0' + n))
+            .ToArray();
+
+        static char[] AllChars = 
+            LowChars.Concat(CapChars).Concat(Numbers).ToArray();
+        #endregion
+
+
+        // پیاده‌سازی مورد استفاده دات‌نت برای پیدا کردن شماره باکت
+        // https://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,bcd13bb775d408f1
+        public static long GetBucketNumber(string str, long bucketCount) =>
+            (str.GetHashCode() & 0x7FFFFFFF) % bucketCount;
+    }
+}
