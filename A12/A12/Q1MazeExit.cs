f4dba4e027791a13caf80264955a1c748d1eb52b
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-12-31 20:46:43 +0330

    project files added

---
+++ A12/A12/Q1MazeExit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A12
+{
+    public class Q1MazeExit : Processor
+    {
+        public Q1MazeExit(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
+        {
+            throw new NotImplementedException();
+        }
+     }
+}
