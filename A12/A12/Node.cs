bf1eed5d8c094cdc3dcaa667d5f6d2e9ef9275b7
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-01-01 23:14:30 +0330

    q1

---
+++ A12/A12/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace A12
+{
+    public class Node
+    {
+        public List<long> Children;
+        public Node()
+        {
+            Children = new List<long>();
+        }
+    }
+}
--- A12/A12/Q1MazeExit.cs
+++ A12/A12/Q1MazeExit.cs
@@ -13,7 +13,34 @@ namespace A12
 
         public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
         {
-            throw new NotImplementedException();
+            bool result = false;
+            bool[] visited = new bool[nodeCount];
+            Node[] nodes = new Node[nodeCount];
+            for (long i = 0; i < edges.Length; i++)
+            {
+                long start = edges[i][0] - 1;
+                long end = edges[i][1] - 1;
+                if (nodes[start] == null)
+                    nodes[start] = new Node();
+                if (nodes[end] == null)
+                    nodes[end] = new Node();
+                nodes[start].Children.Add(end);
+                nodes[end].Children.Add(start);
+            }
+            Queue<long> queue = new Queue<long>();
+            queue.Enqueue(StartNode - 1);
+            long current = 0;
+            while (queue.Count != 0 && !result)
+            {
+                current = (queue.Count != 0) ? queue.Dequeue() : current;
+                visited[current] = true; 
+                if (current == EndNode - 1) { result = true; }
+                if (nodes[current] != null)
+                    foreach (long n in nodes[current].Children)
+                        if (!visited[n])
+                            queue.Enqueue(n);
+            }
+            return (result) ? 1 : 0;
         }
      }
 }
